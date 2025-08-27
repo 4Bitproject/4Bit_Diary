560b21eca604c3e2a24b897d5aae96a41928ce73
JuWon <ehdrbs8751@naver.com>
2025-08-27 14:00:38 +0900

    ✨ Feat.tags.py 업로드

--- app/models/tag.py
+++ app/models/tag.py
@@ -1,12 +1,9 @@
 from tortoise import fields, models
-from tortoise.models import Model
 
-class Tag(Model):
-    tag_id = fields.IntField(pk=True)
-    tag_name = fields.CharField(max_length=50, unique=True)
 
-    # Diaries와 ManyToMany 관계
-    diaries: fields.ManyToManyRelation["Diary"]  # Diary 모델이 나중에 import 되어야 함
+class Tag(models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=50)
 
     class Meta:
-        table = "tags"
+        table = "tag"
