4cbb4be19aad598da36932d83a0d99a49f6588fb
zzinypark <zzinypark21@gamil.com>
2025-08-27 13:38:33 +0900

    ✨ feat : models.users 추가

---
+++ app/models/users.py
@@ -0,0 +1,22 @@
+from tortoise.models import Model
+from tortoise import fields
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    email = fields.CharField(max_length=255,unique=True)
+    password = fields.CharField(max_length=255)
+    nickname = fields.CharField(max_length=50)
+    name = fields.CharField(max_length=50)
+    phone_number = fields.CharField(max_length=20, null=True)
+    last_login = fields.DatetimeField(null=True)
+    is_staff = fields.BooleanField(default=False)
+    is_admin = fields.BooleanField(default=False)
+    is_active = fields.BooleanField(default=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.nickname}({self.name})"
+
+    class Meta:
+        table = "users"
