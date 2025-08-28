9fbd6be014ee965888e874df671bae997c5fcfc0
ing2720 <dlguddns2@naver.com>
2025-08-28 15:12:23 +0900

    🐛 fix : diary.py파일 수정

---
+++ app/models/diaries.py
@@ -0,0 +1,33 @@
+import enum
+
+from tortoise import fields, models
+
+
+class EmotionalState(enum.Enum):
+    JOY = "기쁨"
+    SAD = "슬픔"
+    DEPRESSED = "우울함"
+    ANGRY = "분노"
+
+
+class Diary(models.Model):
+    diary_id = fields.IntField(pk=True)
+    user = fields.ForeignKeyField("models.User", related_name="diaries")
+    title = fields.CharField(max_length=30, null=False)
+    content = fields.CharField(max_length=100)
+    emotional_summary = fields.CharField(max_length=20, null=True)
+    emotional_state = fields.CharEnumField(EmotionalState)
+    created_date = fields.DatetimeField(auto_now_add=True)
+    updated_date = fields.DatetimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class DiaryTag(models.Model):
+    diary = fields.ForeignKeyField("models.Diary", related_name="diary_tags")
+    tag = fields.ForeignKeyField("models.Tag", related_name="diary_tags")
+
+    class Meta:
+        table = "diary_tags"
+        unique_together = [("diary", "tag")]
